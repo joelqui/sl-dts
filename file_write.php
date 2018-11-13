50de0d87bfd3ea3d507c9e79206943f75336bd5f
joelqui <joelkeequi@gmail.com>
2018-11-13 17:06:51 +0800

    sms-2-changes

--- file_write.php
+++ file_write.php
@@ -3,12 +3,35 @@
 // file_put_contents: shortcut for fopen/fwrite/fclose
 // overwrites existing file by default (so be CAREFUL)
 $file = 'sms_outgoing/filetest.sms';
-$number = "639173193264";
-$msg = "you cannot sing the same old romance when there is no more pain.";
+//$number = "639173193264";
+//$number = "639175610034";
+//$number = "639061275582";
+$number = "639178781281";
+$msg = "sl-dts update on doc#181103007: Completed @ OSDS-RECORDS.";
 $content = "To: ".$number."\n\n".$msg." \n";
 //echo $content;
 if($size = file_put_contents($file, $content)) {
   echo "A file of {$size} bytes was created.";
 }
+
+$file = 'sms_outgoing/filetest1.sms';
+
+$msg = "sl-dts update on doc#181103008: Canceled @ OSDS-RECORDS, Please contact the Division Office (053)570-8933.";
+$content = "To: ".$number."\n\n".$msg." \n";
+//echo $content;
+if($size = file_put_contents($file, $content)) {
+  echo "A file of {$size} bytes was created.";
+}
+
+$file = 'sms_outgoing/filetest2.sms';
+
+$msg = "sl-dts update on doc#181103009: Remarks Added @ OSDS-RECORDS, UNABLE TO PROCESS DUE TO OTHER PRESSING MATTERS.";
+$content = "To: ".$number."\n\n".$msg." \n";
+//echo $content;
+if($size = file_put_contents($file, $content)) {
+  echo "A file of {$size} bytes was created.";
+}
+
+
 
 ?>
